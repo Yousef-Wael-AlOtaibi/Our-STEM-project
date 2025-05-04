427669b29ace696ba040a9330b892362ecfad2b2
Yousef-Wael-AlOtaibi <ywalotaibi2010@gmail.com>
2025-05-04 16:05:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const ideaBtn = document.getElementById('idea-btn');
 const stepsBtn = document.getElementById('steps-btn');
 const resultsBtn = document.getElementById('results-btn');
 const selectorBtns = document.querySelectorAll('.selector-btn');
-const backBtn = document.getElementById('back-btn');
+const selectorBtnsContainer = document.getElementById('selector-btns-container')
+const goBackBtn = document.getElementById('go-back-btn');
 const removeBtns = document.querySelectorAll('.remove-btn');
+const clearAndShowAllBtn = document.getElementById('clear-btn')
 const buttonFunctions = {
     'idea-btn':() => {
         document.getElementById('idea').classList.remove('hidden');
@@ -13,32 +15,43 @@ const buttonFunctions = {
     },
     'results-btn':() => {
         document.getElementById('results').classList.remove('hidden')
-    }
+    },
 }
 const relatedButtons = {
-    'remove-idea-btn':'idea-btn',
-    'remove-steps-btn':'steps-btn',
-    'remove-results-btn':'results-btn',
     'idea-btn':'remove-idea-btn',
     'steps-btn':'remove-steps-btn',
     'results-btn':'remove-results-btn'
 }
+const buttonsAndGoToLocations = {
+    'idea-btn': document.getElementById('idea'),
+    'steps-btn': document.getElementById('steps'),
+    'results-btn': document.getElementById('results')
+}
 selectorBtns.forEach(btn => {
     btn.addEventListener('click', () => {
-        const clickedBtnId = document.getElementById(btn.id).id
-         btn.classList.add('hidden');
+        const clickedBtnId = document.getElementById(btn.id).id;
+        const elementToGoTo = buttonsAndGoToLocations[btn.id];
          buttonFunctions[clickedBtnId]();
          const removeBtn = document.getElementById(relatedButtons[btn.id]);
          removeBtn.classList.remove('hidden');
-        
+         elementToGoTo.scrollIntoView({behavior:'smooth', block:'start'});
+          
         })
     });
 removeBtns.forEach(btn => {
     btn.addEventListener('click', () => {
         btn.classList.add('hidden');
         btn.parentElement.classList.add('hidden');
-        const btnToAdd = document.getElementById(relatedButtons[btn.id]);
-        console.log(btnToAdd)
-        btnToAdd.classList.remove('hidden')
     })
+})
+goBackBtn.addEventListener('click', () => {
+    window.scroll({top:0, behavior:"smooth"})
+})
+window.addEventListener('scroll', () => {
+    if(window.scrollY>0 ){
+        goBackBtn.classList.remove('hidden');
+    }
+    else{
+        goBackBtn.classList.add('hidden')
+    }
 })
